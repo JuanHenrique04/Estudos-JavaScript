0c95f920eae95748deba1a111fb9be20dd648648
Juan Henrique <juanmisidoro@gmail.com>
2022-10-30 21:04:45 -0300

    Estudo de funções

---
+++ Estudos/estudosAlura/aluraEx01.js
@@ -0,0 +1,10 @@
+function soma(num1, num2){
+    return num1 + num2
+}
+
+console.log(soma(2, 5))
+
+function multplicacao(num1, num2){
+    return num1 * num2;
+}
+console.log(multplicacao(soma(4, 5)))
